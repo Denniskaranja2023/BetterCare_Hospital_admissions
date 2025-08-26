ae1903b90ee949ccba85bfac8dbe554b7bfa6da1
Denniskaranja2023! <denniswanyeki2021@gmail.com>
2025-08-26 14:40:57 +0300

    add the schema for the classes

---
+++ lib/models.py
@@ -0,0 +1,63 @@
+from sqlalchemy import Column, Integer, String, Text, Date, ForeignKey, create_engine, CheckConstraint,func
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+engine = create_engine("sqlite:///hospital_admissions.db")
+
+#schema for patients table
+class Patient(Base):
+    __tablename__ = "patients"
+    
+    id= Column(Integer(), primary_key=True)
+    full_name= Column(String())
+    gender= Column(String())
+    age=Column(String())
+    reported_condition= Column(Text())
+    doctor_prescription= Column(Text())
+    ward_id=Column(Integer(), ForeignKey)
+    bed_number= Column(Integer())
+    doctor_id= Column(Integer(), ForeignKey)
+    nurse_id= Column(Integer(), ForeignKey)
+    admission_date = Column(Date(), server_default=func.current_date())
+    
+    def __repr__ (self):
+        return f"<Patient_name:{self.full_name}, ward_name:{self.ward.name if self.ward else None}, bed_number:{self.bed_number}>"
+    
+
+#schema for the doctors table
+class Doctor(Base):
+    __tablename__ = 'doctors'
+    
+    id= Column(Integer(), primary_key=True)
+    first_name= Column(String())
+    last_name= Column(String())
+    speciality= Column(String())
+    
+    def __repr__ (self):
+        return f"<Doctor {self.id}: Name:{self.first_name} {self.last_name}, speciality:{self.speciality}"
+
+#schema for the wards table
+class Ward(Base):
+    __tablename__= 'wards'
+    
+    id= Column(Integer(), primary_key=True)
+    ward_name= Column(String())
+    ward_capacity= Column(Integer())
+    ward_location= Column(String())
+    patients= relationship()
+    
+    def __repr__(self):
+        return f"<Ward {self.id}: name:{self.ward_name}, location:{self.ward_location}>"
+
+#schema for the nurses table
+class Nurse(Base):
+    __tablename__ ='nurses'
+    
+    id= Column(Integer(), primary_key=True)
+    first_name= Column(String())
+    last_name=Column(String())
+    gender=Column(String())
+    
+    def __repr__(self):
+        return f"<Nurse {self.id}: name: {self.first_name} {self.last_name},gender: {self.gender}"
